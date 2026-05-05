609f7479d8dc488dd127ddabdd6c659bc0cdb0d1
Fola <folajimibabslawal@gmail.com>
2026-05-05 23:13:53 +0100

    qed

---
+++ src/main/java/org/fola/data/models/ExamAttempt.java
@@ -0,0 +1,21 @@
+package org.fola.data.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "exam_attempts")
+public class ExamAttempt {
+    @Id
+    private String id;
+    private String studentId;
+    private String examId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Double score;
+    private Double totalPoints;
+    private AttemptStatus status;
+}
