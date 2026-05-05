750c919381ffff5a08acae3bfb2cf8a65c3b1e33
Fola <folajimibabslawal@gmail.com>
2026-05-05 22:20:25 +0100

    qed

--- src/main/java/org/fola/data/models/User.java
+++ src/main/java/org/fola/data/models/User.java
@@ -1,4 +1,22 @@
 package org.fola.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collation = "users")
 public class User {
+    @Id
+    private String id;
+    private String name;
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+    private Role role;
+    private boolean active;
+    private LocalDateTime createdAt;
 }
